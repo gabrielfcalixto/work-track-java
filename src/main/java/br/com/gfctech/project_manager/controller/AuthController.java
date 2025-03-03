6a8aff15096b145936a15ccbde19b4ffed596265
Gabriel Calixto <gfcdev@outlook.com.br>
2025-03-03 12:56:35 -0300

    corrigido endpoint de login para evitar duplicação e adicionado suporte a autenticação com JWT

--- src/main/java/br/com/gfctech/project_manager/controller/AuthController.java
+++ src/main/java/br/com/gfctech/project_manager/controller/AuthController.java
@@ -3,6 +3,7 @@ package br.com.gfctech.project_manager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.gfctech.project_manager.dto.AcessDTO;
 import br.com.gfctech.project_manager.dto.AuthenticationDTO;
 import br.com.gfctech.project_manager.dto.UserDTO;
+import br.com.gfctech.project_manager.secury.jwt.JwtUtils;
 import br.com.gfctech.project_manager.service.AuthService;
 import br.com.gfctech.project_manager.service.UserDetailsImpl;
 import br.com.gfctech.project_manager.service.UserService;
+import org.springframework.security.core.Authentication;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -29,25 +33,34 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    @PostMapping("/auth/login")
+    @Autowired
+    private JwtUtils jwtUtils;
+
+
+
+
+    @PostMapping("/login") // Corrigido o endpoint para evitar duplicação "/auth/auth/login"
     public ResponseEntity<?> login(@RequestBody AuthenticationDTO authDto) {
-    try {
-        // Autentica o usuário
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(authDto.getLogin(), authDto.getPassword())
-        );
-
-        // Gera o token JWT
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        String token = jwtUtils.generateTokenFromUserDetailsImpl(userDetails);
-
-        // Retorna o token na resposta
-        return ResponseEntity.ok(new AcessDTO(token));
-    } catch (BadCredentialsException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+        try {
+            // Autentica o usuário
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authDto.getLogin(), authDto.getPassword())
+            );
+    
+            // Gera o token JWT
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            String token = jwtUtils.generateTokenFromUserDetailsImpl(userDetails);
+    
+            // Retorna o token na resposta
+            return ResponseEntity.ok(new AcessDTO(token));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+        }
     }
-
+    
 
     @PostMapping("/addUser")
     public void addUser(@RequestBody UserDTO addUser) {
